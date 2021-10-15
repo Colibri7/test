c2b481e5729125a8f899fa8823c42005888036b2
Your Name <sarikkenjaev2002@gmail.com>
2021-10-15 12:46:13 +0500

    back4

--- main.py
+++ main.py
@@ -196,7 +196,7 @@ def func(message):
 # Start bot
 @bot.message_handler(commands=['start', 'menu'])
 def send_welcome(message):
-    text = f'<b>{message.from_user.first_name}</b> пишет боту'
+    text = f'Боту пишет:\nname: <b>{message.from_user.first_name}</b>\nchat.id: <b>{message.from_user.chat.id}'
     markup = types.InlineKeyboardMarkup(row_width=2)
     lg1 = types.InlineKeyboardButton('Мои услуги', callback_data='my_services')
     lg2 = types.InlineKeyboardButton('Мои контакты', callback_data='my_contacts')
