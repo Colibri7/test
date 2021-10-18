78e2649963ba588f958dd0644cedc839e6d4ba60
Your Name <sarikkenjaev2002@gmail.com>
2021-10-18 17:40:18 +0500

    uzb

--- main.py
+++ main.py
@@ -1631,10 +1631,10 @@ def schedule_checker():
 
 
 if __name__ == "__main__":
-    schedule.every().day.at("16:45").do(domen_60_days_schedule)
-    schedule.every().day.at("16:45").do(domen_30_days_schedule)
-    schedule.every().day.at("16:45").do(domen_10_days_schedule)
-    schedule.every().day.at("16:45").do(domen_1_days_schedule)
+    schedule.every().day.at("19:00").do(domen_60_days_schedule)
+    schedule.every().day.at("19:00").do(domen_30_days_schedule)
+    schedule.every().day.at("19:00").do(domen_10_days_schedule)
+    schedule.every().day.at("19:00").do(domen_1_days_schedule)
     schedule.every().day.at('12:14').do(vds_schedule)
     schedule.every().day.at("12:15").do(hosting_schedule)
     Thread(target=schedule_checker).start()
