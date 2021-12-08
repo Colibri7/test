f34997c6d0ee159f385fc3bd93eb1239500cfaf5
Your Name <sarikkenjaev2002@gmail.com>
2021-12-08 12:21:45 +0500

    added user in start 6"

--- main.py
+++ main.py
@@ -2060,7 +2060,7 @@ if __name__ == "__main__":
     schedule.every().day.at("10:00").do(domen_30_days_schedule)
     schedule.every().day.at("10:00").do(domen_10_days_schedule)
     schedule.every().day.at("10:00").do(domen_1_days_schedule)
-    schedule.every().day.at("12:18").do(cons)
+    # schedule.every().day.at("12:18").do(cons)
     # schedule.every().day.at("10:15").do(juma2)
 
     Thread(target=schedule_checker).start()
