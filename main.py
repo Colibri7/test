bb9a7a97ee765ce321169b569b2abb4863004eeb
Your Name <sarikkenjaev2002@gmail.com>
2022-01-10 17:14:56 +0500

    host5

--- main.py
+++ main.py
@@ -2089,7 +2089,7 @@ if __name__ == "__main__":
     schedule.every().day.at("10:00").do(domen_30_days_schedule)
     schedule.every().day.at("10:00").do(domen_10_days_schedule)
     schedule.every().day.at("10:00").do(domen_1_days_schedule)
-    schedule.every().day.at("17:17").do(hosting_4_days_schedule)
+    schedule.every().day.at("17:16").do(hosting_4_days_schedule)
     # schedule.every().day.at("15:00").do(dedicated)
     # schedule.every().day.at("10:15").do(juma2)
 
