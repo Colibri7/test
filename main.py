d89012206bfabe6fc9ce61e1b0c7b8734a9dbc15
Your Name <sarikkenjaev2002@gmail.com>
2022-02-18 14:18:02 +0500

    asd

--- main.py
+++ main.py
@@ -1849,7 +1849,7 @@ def log_uz(message):
         username = message.chat.username
         timestamp = message.date
         # dt_obj = datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
-        dt_obj = time.strptime(timestamp, '%Y-%m-%d %H:%M:%S')
+        dt_obj = time.strptime(str(timestamp), '%Y-%m-%d %H:%M:%S')
         connection = pymysql.connect(host='62.209.143.131',
                                      user='hostmasteruz_pbot',
                                      password='bcaxoZyAXDGc',
